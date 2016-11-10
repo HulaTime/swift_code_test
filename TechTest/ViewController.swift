ac46573e5e521d8b1041436503a560d151bee9ba
Max Provin <maxprovin@outlook.com>
2016-11-10 23:45:31 +0000

    Initial Commit

---
+++ TechTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TechTest
+//
+//  Created by Luiz Provin on 10/11/2016.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
