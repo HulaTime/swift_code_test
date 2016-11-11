bd61595fc64525208eed906d307185e0f540f806
Max Provin <maxprovin@outlook.com>
2016-11-11 00:15:41 +0000

    project initialized

--- TechTest/ViewController.swift
+++ TechTest/ViewController.swift
@@ -13,6 +13,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+          let requestURL: NSURL = NSURL(string: "http://www.learnswiftonline.com/Samples/subway.json")!
+          let urlRequest: NSMutableURLRequest = NSMutableURLRequest(url: requestURL as URL)
+          let session = URLSession.shared
+          let task = session.dataTask(with: urlRequest as URLRequest) {
+              (data, response, error) -> Void in
+            
+              let httpResponse = response as! HTTPURLResponse
+              let statusCode = httpResponse.statusCode
+            
+              if (statusCode == 200) {
+                  print("Everyone is fine, file downloaded successfully.")
+              }
+          }
+      
+          task.resume()
     }
 
     override func didReceiveMemoryWarning() {
